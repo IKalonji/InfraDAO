b6089c1f19277c0a544c30f570e138da64c06301
IKalonji <ikalonji@student.wethinkcode.co.za>
2023-06-18 10:34:15 +0200

    project submition completed

--- src/Components/Navbar.componet.js
+++ src/Components/Navbar.componet.js
@@ -1,75 +1,76 @@
 import React from 'react';
-//import { useRouter } from 'next/router';
-import { Toolbar } from 'primereact/toolbar';
 import { Button } from 'primereact/button';
 import { SplitButton } from 'primereact/splitbutton';
+import { Toolbar } from 'primereact/toolbar';
+import Typography from '@mui/material/Typography';
 
-import Image from './Assets/ai.jpg'
+import Image from './Assets/TopIcon.png';
 
 class Navbar extends React.Component {
-    //const router = useRouter();
-    GoBackHome(){
-        window.location.href = "/"
-    }
-    GoToSUbmit(){
-        window.location.href = '/submit'
-    }
-    GoToView(){
-        window.location.href = '/user/view-projects'
-    }
-    GoToDexPage(){
-        window.location.href = '/dex-page'
-    }
+  GoBackHome() {
+    window.location.href = "/";
+  }
 
-    items = [
-        {
-            label: 'DAO MEmbers',
-            icon: 'pi pi-external-link',
-            command: () => {
-                window.location.href = '/member/view-projects'
-            }
-        },
-        {
-            label: 'Join DAO',
-            icon: 'pi pi-refresh',
-            command: () => {
-                window.location.href = '/joinDAO'
-            }
-        },
-    ];
+  GoToSubmit() {
+    window.location.href = '/submit';
+  }
 
-    startContent = (
-        <React.Fragment>
-            {/* <Button label="Home" icon="pi pi-home" className="p-button-outlined"  onClick={this.GoBackHome}/> */}
-            <Button className="p-button-text" onClick={this.GoBackHome} style={{width:"290px"}}>
-                <img src={Image} alt='' style={{width:"28%", height:"12.5%", borderRadius:"25%"}}/>
-                <h2>InfraDAO</h2>
-            </Button>
+  GoToView() {
+    window.location.href = '/user/view-projects';
+  }
 
-            {/* <div onClick={this.GoBackHome} style={{cursor:"pointer", gap:"14px" }}>
-                <img src={Image} alt='' style={{ width: '10%', height: '5%', borderRadius: '25%' }} />
-                <h2>InfraDAO</h2>
-            </div> */}
-        </React.Fragment>
+  GoToDexPage() {
+    window.location.href = '/dex-page';
+  }
+
+  items = [
+    {
+      label: 'DAO Members',
+      icon: 'pi pi-external-link',
+      command: () => {
+        window.location.href = '/member/view-projects';
+      }
+    },
+    {
+      label: 'Join DAO',
+      icon: 'pi pi-refresh',
+      command: () => {
+        window.location.href = '/joinDAO';
+      }
+    },
+  ];
+
+  startContent = (
+    // <Button className="p-button-text" onClick={this.GoBackHome} >
+    //   <img src={Image} alt='' style={{ width: "28%", height: "42.5%", borderRadius: "25%" }} />
+    //   InfraDAO
+    // </Button>
+    <a href="/" style={{ textDecoration: 'none', color: 'black' }}>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <img src={Image} alt="" style={{ width: '25%', height: '50px', marginRight: '10px' }} />
+      <Typography variant="h6" noWrap>
+        InfraDAO
+      </Typography>
+    </div>
+  </a>
+  );
+
+  endContent = (
+    <>
+      <Button label="Submit project" icon="pi pi-plus" className="mr-2" onClick={this.GoToSubmit} />
+      <Button label="View" icon="pi pi-image" className="mr-2" onClick={this.GoToView} />
+      <SplitButton label="DAO Members" icon="pi pi-view" model={this.items} className="mr-2"></SplitButton>
+      <Button label="Dex" className="mr-2" onClick={this.GoToDexPage} />
+    </>
+  );
+
+  render() {
+    return (
+      <div className="card">
+        <Toolbar start={this.startContent} end={this.endContent} />
+      </div>
     );
-
-    endContent = (
-        <React.Fragment>
-           
-            <Button label="Submit project" icon="pi pi-plus" className="mr-2" onClick={this.GoToSUbmit}/>
-            <Button label="View" icon="pi pi-image" className="mr-2" onClick={this.GoToView} />
-            <SplitButton label="DAO Members" icon="pi pi-view" model={this.items} className="mr-2"></SplitButton>
-            <Button label="Dex" className="mr-2" onClick={this.GoToDexPage} />
-        </React.Fragment>
-    );
-
-    render() {
-        return (
-            <div className="card">
-                <Toolbar start={this.startContent} end={this.endContent} />
-            </div>
-        );
-    }
+  }
 }
 
 export default Navbar;
--- src/pages/Submit-Project.pages.js
+++ src/pages/Submit-Project.pages.js
@@ -17,7 +17,7 @@ export default class SubmitProject extends Component {
       submitorAddress: '',
       projectName: '',
       projectDetails: '',
-      projectCost: 0,
+      projectCost: '',
       raiseAmount: '',
       tokenization: '',
       initPricePerToken: '',
@@ -45,10 +45,25 @@ export default class SubmitProject extends Component {
   }
 
   //All changes for the input fiels
+  // handleChange = (e) => {
+  //   const { id, value } = e.target;
+  //   this.setState({ [id]: value });
+  // }
+
   handleChange = (e) => {
     const { id, value } = e.target;
-    this.setState({ [id]: value });
+    let updatedState = { [id]: value };
+  
+    if (id === "ExpectedBuyBack" || id === "initPricePerToken") {
+      const { ExpectedBuyBack, initPricePerToken } = this.state;
+      const ROI = ((parseFloat(ExpectedBuyBack) - parseFloat(initPricePerToken)) / parseFloat(initPricePerToken)) * 100;
+      updatedState.ReturnOnInvestment = ROI.toFixed(2);
+    }
+  
+    this.setState(updatedState);
   }
+  
+  
 
   
   //Once the button is clicked this is the function called
@@ -126,92 +141,90 @@ export default class SubmitProject extends Component {
     const header = this.renderHeader();
     // const { selectedCategory } = this.state;
     return (
-      <div>
-        <Toast ref={(el) => (this.toast = el)} />
-          <div style={{ height: "60px" }}></div>
+    <div>
+      <Toast ref={(el) => (this.toast = el)} />
+      <div style={{ height: "60px" }}></div>
+        <div className="flex align-items-center justify-content-center">
+          <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6">
+            <div className="text-center mb-5">
+              <img src={Image} alt="hyper" height={50} className="mb-3"  style={{borderRadius:"25%"}}/>
+              <div className="text-900 text-3xl font-medium mb-3">Submit Your Project</div>
+            </div>
+              <div>
+                <label htmlFor="SubmitorAddress" className="block text-900 font-medium mb-2">Submitor</label>
+                <InputText id="submitorAddress" type="text" placeholder="Submitor Address" className="w-full mb-3" onChange={this.handleChange} value={submitorAddress} />
+                <div style={{ height: "12px" }}></div>
 
-          <div className="flex align-items-center justify-content-center">
-            <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6">
-              <div className="text-center mb-5">
-                <img src={Image} alt="hyper" height={50} className="mb-3"  style={{borderRadius:"25%"}}/>
-                <div className="text-900 text-3xl font-medium mb-3">Submit Your Project</div>
-              </div>
-                <div>
-                  <label htmlFor="SubmitorAddress" className="block text-900 font-medium mb-2">Submitor</label>
-                  <InputText id="submitorAddress" type="text" placeholder="Submitor Address" className="w-full mb-3" onChange={this.handleChange} value={submitorAddress} />
-                  <div style={{ height: "12px" }}></div>
+                <label htmlFor="ProjectName" className="block text-900 font-medium mb-2">Project Name</label>
+                <div style={{height:"13px"}}></div>
+                <InputText id="projectName" type="text" placeholder="Project Name" className="w-full mb-3" onChange={this.handleChange} value={projectName} />
+                <div style={{ height: "12px" }}></div>
 
-                  <label htmlFor="ProjectName" className="block text-900 font-medium mb-2">Project Name</label>
-                  <div style={{height:"13px"}}></div>
-                  <InputText id="projectName" type="text" placeholder="Project Name" className="w-full mb-3" onChange={this.handleChange} value={projectName} />
-                  <div style={{ height: "12px" }}></div>
-
-                  
-                  <label htmlFor="SelectCategory" className="block text-900 font-medium w-full">Select Project Category</label>
-                  <MultiSelect value={selectedCategory} onChange={this.handleMultiSelectChange} options={this.InfrastructureOptions} optionLabel="name"
-                    placeholder="Select Infrastructure Category"  className="w-full md:w-20rem" />
-                  
-                  <div style={{ height: "12px" }}></div>
                 
-                  <div className="card">
-                      <label htmlFor="ProjectDetails" className="block text-900 font-medium mb-2">Project Details</label>
-                      <Editor headerTemplate={header} style={{ height: '320px' }} value={projectDetails} onTextChange={(e) => this.setState({ projectDetails: e.htmlValue })} />
-                  </div>
-                  <div style={{ height: "12px" }}></div>
+                <label htmlFor="SelectCategory" className="block text-900 font-medium w-full">Select Project Category</label>
+                <MultiSelect value={selectedCategory} onChange={this.handleMultiSelectChange} options={this.InfrastructureOptions} optionLabel="name"
+                  placeholder="Select Infrastructure Category"  className="w-full md:w-20rem" />
+                
+                <div style={{ height: "12px" }}></div>
+              
+                <div className="card">
+                    <label htmlFor="ProjectDetails" className="block text-900 font-medium mb-2">Project Details</label>
+                    <Editor headerTemplate={header} style={{ height: '320px' }} value={projectDetails} onTextChange={(e) => this.setState({ projectDetails: e.htmlValue })} />
+                </div>
+                <div style={{ height: "12px" }}></div>
 
-                  <label htmlFor="FileUpload" className="block text-900 font-medium mb-2">Upload Plans</label>
-                  {/* <FileUpload name="demo[]" multiple accept="image/*" mode="basic"  /> */}
-                  <FileUpload name="PlanUpload" url={'/api/upload'} multiple 
-                  accept="image/*" 
-                  emptyTemplate={<p className="m-0">Upload all your project plans </p>} 
-                  onUpload={(e) => this.setState({ files: e.files })}/>
+                <label htmlFor="FileUpload" className="block text-900 font-medium mb-2">Upload Plans</label>
+                <FileUpload name="PlanUpload" url={'/api/upload'} multiple 
+                accept="image/*" 
+                emptyTemplate={<p className="m-0">Upload all your project plans </p>} 
+                onUpload={(e) => this.setState({ files: e.files })}/>
 
-                  <div style={{ height: "12px" }}></div>
+                <div style={{ height: "12px" }}></div>
 
-                  <div>
-                      <label htmlFor="FinancialsUpload" className="block text-900 font-medium mb-2">Upload Financials</label>
-                      <FileUpload name="financials" url={'/api/upload'} multiple 
-                      accept="*" 
-                      emptyTemplate={<p className="m-0">Upload your financials here</p>} 
-                      onUpload={(e) => this.setState({ financials: e.files })}/>
-                  </div>
-                  <div style={{ height: "12px" }}></div>
+                <div>
+                    <label htmlFor="FinancialsUpload" className="block text-900 font-medium mb-2">Upload Financials</label>
+                    <FileUpload name="financials" url={'/api/upload'} multiple 
+                    accept="*" 
+                    emptyTemplate={<p className="m-0">Upload your financials here</p>} 
+                    onUpload={(e) => this.setState({ financials: e.files })}/>
+                </div>
+                <div style={{ height: "12px" }}></div>
 
-                  <label htmlFor="ProjectCost" className="block text-900 font-medium mb-2">Project Cost</label>
-                  <InputNumber id="projectCost" placeholder="Project Cost" className="w-full mb-3" onChange={this.handleChange} value={projectCost} />
-                  <div style={{ height: "12px" }}></div>
+                <label htmlFor="ProjectCost" className="block text-900 font-medium mb-2">Project Cost</label>
+                <InputText id="projectCost" placeholder="Project Cost" className="w-full mb-3" onChange={this.handleChange} value={projectCost} keyfilter="int" />
+                <div style={{ height: "12px" }}></div>
 
-                  <label htmlFor="RaiseAmount" className="block text-900 font-medium mb-2">Raise Amount Required</label>
-                  <InputText id="raiseAmount" inputMode="numeric" placeholder="Raise amount required" className="w-full mb-3" onChange={this.handleChange} value={raiseAmount} />
-                  <div style={{ height: "12px" }}></div>
+                <label htmlFor="RaiseAmount" className="block text-900 font-medium mb-2">Raise Amount Required</label>
+                <InputText id="raiseAmount" inputMode="numeric" placeholder="Raise amount required" className="w-full mb-3" onChange={this.handleChange} value={raiseAmount} keyfilter="int" />
+                <div style={{ height: "12px" }}></div>
 
-                  <label htmlFor="TotalTokenAmount" className="block text-900 font-medium mb-2">Total Token Amount</label>
-                  <InputText id="tokenization" type="text" placeholder="Total token amount" className="w-full mb-3" onChange={this.handleChange} value={tokenization} />
-                  <div style={{ height: "12px" }}></div>
+                <label htmlFor="TotalTokenAmount" className="block text-900 font-medium mb-2">Total Token Amount</label>
+                <InputText id="tokenization" type="text" placeholder="Total token amount" className="w-full mb-3" onChange={this.handleChange} value={tokenization}  keyfilter="int"/>
+                <div style={{ height: "12px" }}></div>
 
-                  <label htmlFor="InitPrice" className="block text-900 font-medium mb-2">Initial Price per Token</label>
-                  <InputText id="initPricePerToken" type="text" placeholder="Initial Price per Token" className="w-full mb-3" onChange={this.handleChange} value={initPricePerToken} />
-                  <div style={{ height: "12px" }}></div>
+                <label htmlFor="InitPrice" className="block text-900 font-medium mb-2">Initial Price per Token</label>
+                <InputText id="initPricePerToken" type="text" placeholder="Initial Price per Token" className="w-full mb-3" onChange={this.handleChange} value={initPricePerToken} keyfilter="int" />
+                <div style={{ height: "12px" }}></div>
 
-                  <label htmlFor="ExpectedBuyBack" className="block text-900 font-medium mb-2">Expected Token Buy Back {"(After project completion)"}</label>
-                  <InputText id="ExpectedBuyBack" type="text" placeholder="Expected Token Buy Back" className="w-full mb-3" onChange={this.handleChange} value={ExpectedBuyBack} />
-                  <div style={{ height: "12px" }}></div>
+                <label htmlFor="ExpectedBuyBack" className="block text-900 font-medium mb-2">Expected Token Buy Back {"(After project completion)"}</label>
+                <InputText id="ExpectedBuyBack" type="text" placeholder="Expected Token Buy Back" className="w-full mb-3" onChange={this.handleChange} value={ExpectedBuyBack} keyfilter="int" />
+                <div style={{ height: "12px" }}></div>
 
-                  <label htmlFor="ReturnOnInvestment" className="block text-900 font-medium mb-2"> Return On Investment </label>
-                  <InputText id="ReturnOnInvestment" type="text" placeholder="ROI" className="w-full mb-3" onChange={this.handleChange} value={ReturnOnInvestment} />
-                  <div style={{ height: "12px" }}></div>
+                <label htmlFor="ReturnOnInvestment" className="block text-900 font-medium mb-2"> Return On Investment </label>
+                <InputText id="ReturnOnInvestment" type="text" placeholder="ROI" className="w-full mb-3" onChange={this.handleChange} readOnly value={ReturnOnInvestment} keyfilter="int" />
+                <div style={{ height: "12px" }}></div>
 
-                  <label htmlFor="ContractedDevelopers" className="block text-900 font-medium mb-2">Contracted Developers</label>
-                  <InputText id="contractedDevelopers" type="text" placeholder="Contracted Developers" className="w-full mb-3" onChange={this.handleChange} value={contractedDevelopers} />
-                  <div style={{ height: "12px" }}></div>
+                <label htmlFor="ContractedDevelopers" className="block text-900 font-medium mb-2">Contracted Developers</label>
+                <InputText id="contractedDevelopers" type="text" placeholder="Contracted Developers" className="w-full mb-3" onChange={this.handleChange} value={contractedDevelopers} />
+                <div style={{ height: "12px" }}></div>
 
-                  <Button label="Submit Your Project" icon="pi pi-cloud-upload" className="w-full" onClick={this.handleButtonClick} />
-                  </div>
-              </div>
-          </div>
+                <Button label="Submit Your Project" icon="pi pi-cloud-upload" className="w-full" onClick={this.handleButtonClick} />
+                </div>
+            </div>
+        </div>
 
-          <div style={{ height: "40px" }}></div>
-      </div>
-      );
+        <div style={{ height: "40px" }}></div>
+    </div>
+    );
   }
 }
